3492b1ab6ae15d342e05b54d4892356928db4fad
Sudha <sudha@xmos.com>
2013-11-15 12:35:28 +0530

    included display spectrum from adc

--- app_display_controller_demo/doc_quickstart/_build/.sources/app_display_controller_demo/src/chan.h
+++
@@ -1,5 +0,0 @@
-#include <xs1.h>
-#ifndef CHAN_H_
-#define CHAN_H_
-void send(chanend c, short d);
-#endif /* CHAN_H_ */
--- app_display_controller_demo/doc_quickstart/_build/.sources/app_display_controller_demo/src/loader.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef LOADER_H_
-#define LOADER_H_
-
-#ifndef __XC__
-#include "xccompat.h"
-#endif
-void loader(chanend c, char images[][15], unsigned image_count);
-#endif /* LOADER_H_ */
