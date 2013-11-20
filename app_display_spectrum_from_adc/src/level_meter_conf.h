2d9100119ffc462aaeae1d3dd1e2328441bda80f
Sudha <sudha@xmos.com>
2013-11-20 13:54:42 +0530

    removed display spectrum from adc

--- app_display_spectrum_from_adc/src/level_meter_conf.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef LEVEL_METER_CONF_H_
-#define LEVEL_METER_CONF_H_
-
-#define LEVEL_METER_NCOLORS 5
-#define LEVEL_METER_COLORS {RED,GREEN,MAGENTA,CYAN,CHOCOLATE}	//Pick colors from the list in lever_meter.h
-
-#endif /* LEVEL_METER_CONF_H_ */
